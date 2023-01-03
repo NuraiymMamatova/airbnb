90566633eb6d3f092107eaa2b1e449bd08ad1473
Nurisa <nurisa.abdykalykova03@gmail.com>
2023-01-03 14:20:36 +0600

    add files

---
+++ src/main/java/com/example/airbnbb7/AirbnbB7Application.java
@@ -0,0 +1,13 @@
+package com.example.airbnbb7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbnbB7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirbnbB7Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/airbnbb7/AirbnbB7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airbnbb7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbB7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
