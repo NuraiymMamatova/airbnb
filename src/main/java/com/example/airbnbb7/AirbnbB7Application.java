b7d5c1a09c582264b3a0b237a17cc2ca1c8378f3
Nurisa <nurisa.abdykalykova03@gmail.com>
2023-01-03 14:49:29 +0600

    add introduction page

--- src/main/java/com/example/airbnbb7/AirbnbB7Application.java
+++ src/main/java/com/example/airbnbb7/AirbnbB7Application.java
@@ -2,12 +2,19 @@ package com.example.airbnbb7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class AirbnbB7Application {
 
     public static void main(String[] args) {
         SpringApplication.run(AirbnbB7Application.class, args);
     }
 
+    @GetMapping("/")
+    public String greetings(){
+        return "introduction";
+    }
 }
