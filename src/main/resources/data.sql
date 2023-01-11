56f0a8ba4b5d2225f7db0d7916352f5f9924c952
Eldiar004 <eldiarro2004@gmail.com>
2023-01-11 18:19:56 +0600

    finished data init

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,84 @@
-insert into Roles(id , nameOfRole)
-values (1 , 'ADMIN');
-insert into Houses(id,price,title,descriptionOfListing,maxOfGuests)
-values
+insert into role(id,name_of_role)
+values (1,'Admin');
+
+insert into users(id,name,email,password,image,count_of_booked_house)
+values (1,'Eldiar','eldiarro2004@gmail.com','eldiar',null,null);
+
+insert into roles_users(role_id, user_id)
+VALUES(1,1);
+
+insert into houses(id,price,title,description_of_listing,max_of_guests,count_of_booked_user,
+                   house_type,houses_status,houses_booked,date_house_created,location_id
+                   ,owner_id)
+values(1,20,null,null,5,0,0,2,1,null,null,1);
+
+
+insert into houses(id,price,title,description_of_listing,max_of_guests,count_of_booked_user,
+                   house_type,houses_status,houses_booked,date_house_created,location_id
+    ,owner_id)
+values(2,20,null,null,5,0,0,2,1,null,null,1);
+
+insert into houses(id,price,title,description_of_listing,max_of_guests,count_of_booked_user,
+                   house_type,houses_status,houses_booked,date_house_created,location_id
+    ,owner_id)
+values(3,20,null,null,5,0,0,2,1,null,null,1);
+
+insert into houses(id,price,title,description_of_listing,max_of_guests,count_of_booked_user,
+                   house_type,houses_status,houses_booked,date_house_created,location_id
+    ,owner_id)
+values(4,20,null,null,5,0,0,2,1,null,null,1);
+
+insert into houses(id,price,title,description_of_listing,max_of_guests,count_of_booked_user,
+                   house_type,houses_status,houses_booked,date_house_created,location_id
+    ,owner_id)
+values(5,20,null,null,5,0,0,2,1,null,null,1);
+
+insert into houses(id,price,title,description_of_listing,max_of_guests,count_of_booked_user,
+                   house_type,houses_status,houses_booked,date_house_created,location_id
+    ,owner_id)
+values(6,20,null,null,5,0,0,2,1,null,null,1);
+
+insert into houses(id,price,title,description_of_listing,max_of_guests,count_of_booked_user,
+                   house_type,houses_status,houses_booked,date_house_created,location_id
+    ,owner_id)
+values(7,20,null,null,5,0,0,2,1,null,null,1);
+
+insert into houses(id,price,title,description_of_listing,max_of_guests,count_of_booked_user,
+                   house_type,houses_status,houses_booked,date_house_created,location_id
+    ,owner_id)
+values(8,20,null,null,5,0,0,2,1,null,null,1);
+
+insert into houses(id,price,title,description_of_listing,max_of_guests,count_of_booked_user,
+                   house_type,houses_status,houses_booked,date_house_created,location_id
+    ,owner_id)
+values(9,20,null,null,5,0,0,2,1,null,null,1);
+
+insert into houses(id,price,title,description_of_listing,max_of_guests,count_of_booked_user,
+                   house_type,houses_status,houses_booked,date_house_created,location_id
+    ,owner_id)
+values(10,20,null,null,5,0,0,2,1,null,null,1);
+
+insert into houses(id,price,title,description_of_listing,max_of_guests,count_of_booked_user,
+                   house_type,houses_status,houses_booked,date_house_created,location_id
+    ,owner_id)
+values(11,20,null,null,5,0,0,2,1,null,null,1);
+
+insert into houses(id,price,title,description_of_listing,max_of_guests,count_of_booked_user,
+                   house_type,houses_status,houses_booked,date_house_created,location_id
+    ,owner_id)
+values(12,20,null,null,5,0,0,2,1,null,null,1);
+
+insert into houses(id,price,title,description_of_listing,max_of_guests,count_of_booked_user,
+                   house_type,houses_status,houses_booked,date_house_created,location_id
+    ,owner_id)
+values(13,20,null,null,5,0,0,2,1,null,null,1);
+
+insert into houses(id,price,title,description_of_listing,max_of_guests,count_of_booked_user,
+                   house_type,houses_status,houses_booked,date_house_created,location_id
+    ,owner_id)
+values(15,20,null,null,5,0,0,2,1,null,null,1);
+
+insert into houses(id,price,title,description_of_listing,max_of_guests,count_of_booked_user,
+                   house_type,houses_status,houses_booked,date_house_created,location_id
+    ,owner_id)
+values(16,20,null,null,5,0,0,2,1,null,null,1);
