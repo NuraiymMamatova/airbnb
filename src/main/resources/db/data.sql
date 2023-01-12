5458764d6f1782452e5ed2bf0648e9e9983f55e5
Eldiar004 <eldiarro2004@gmail.com>
2023-01-12 13:37:02 +0600

    again fixed data.sql and added mapped by in house for location

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -14,35 +14,29 @@ VALUES(1,1);
 
 
 insert into houses(id,price,title,description_of_listing,max_of_guests,count_of_booked_user,
-                   house_type,houses_status,houses_booked,date_house_created,location_id
-    ,owner_id)
+                   house_type,houses_status,houses_booked,date_house_created,owner_id)
 values(1,20,'Asman','The hotel will provide guests with air-conditioned rooms offering a desk, a kettle, a fridge, a minibar, a safety deposit box, a flat-screen TV and a shared bathroom with a shower. At Garden Hotel & SPA the rooms have bed linen and towels.
-',5,0,0,2,1,now(),null,1);
+',5,0,0,2,1,null,1);
 
 insert into houses(id,price,title,description_of_listing,max_of_guests,count_of_booked_user,
-                   house_type,houses_status,houses_booked,date_house_created,location_id
-    ,owner_id)
+                   house_type,houses_status,houses_booked,date_house_created,owner_id)
 values(2,15,'Ihlas','The hotel will provide guests with air-conditioned rooms offering a desk, a kettle, a fridge, a minibar, a safety deposit box, a flat-screen TV and a shared bathroom with a shower. At Garden Hotel & SPA the rooms have bed linen and towels.
-',10,1,0,2,1,now(),null,2);
+',10,1,0,2,1,null,1);
 
 insert into houses(id,price,title,description_of_listing,max_of_guests,count_of_booked_user,
-                   house_type,houses_status,houses_booked,date_house_created,location_id
-    ,owner_id)
+                   house_type,houses_status,houses_booked,date_house_created,owner_id)
 values(3,15,'Elite','The hotel will provide guests with air-conditioned rooms offering a desk, a kettle, a fridge, a minibar, a safety deposit box, a flat-screen TV and a shared bathroom with a shower. At Garden Hotel & SPA the rooms have bed linen and towels.
-',10,1,0,2,1,now(),null,2);
+',10,1,0,2,1,null,1);
 
 insert into houses(id,price,title,description_of_listing,max_of_guests,count_of_booked_user,
-                   house_type,houses_status,houses_booked,date_house_created,location_id
-    ,owner_id)
+                   house_type,houses_status,houses_booked,date_house_created,owner_id)
 values(4,15,'Delmar','The hotel will provide guests with air-conditioned rooms offering a desk, a kettle, a fridge, a minibar, a safety deposit box, a flat-screen TV and a shared bathroom with a shower. At Garden Hotel & SPA the rooms have bed linen and towels.
-',10,1,0,2,1,now(),null,1);
+',10,1,0,2,1,null,2);
 
 insert into houses(id,price,title,description_of_listing,max_of_guests,count_of_booked_user,
-                   house_type,houses_status,houses_booked,date_house_created,location_id
-    ,owner_id)
+                   house_type,houses_status,houses_booked,date_house_created,owner_id)
 values(5,15,'Best','The hotel will provide guests with air-conditioned rooms offering a desk, a kettle, a fridge, a minibar, a safety deposit box, a flat-screen TV and a shared bathroom with a shower. At Garden Hotel & SPA the rooms have bed linen and towels.
-',10,1,0,2,1,now(),null,1);
-
+',10,1,0,2,1,null,2);
 
 
 
@@ -50,16 +44,16 @@ insert into location(id, address, region, town_or_province, house_id)
 values (1,'Toktogul 12','Bishkek','Goin',1);
 
 insert into location(id, address, region, town_or_province, house_id)
-values (1,'Kurmanjan 36','Osh','Samsa',2);
+values (2,'Kurmanjan 36','Osh','Samsa',2);
 
 insert into location(id, address, region, town_or_province, house_id)
-values (1,'Saburov 19','Bishkek','Vefa',3);
+values (3,'Saburov 19','Bishkek','Vefa',3);
 
 insert into location(id, address, region, town_or_province, house_id)
-values (1,'Pushkin 12','Bishkek','Red',4);
+values (4,'Pushkin 12','Bishkek','Red',4);
 
 insert into location(id, address, region, town_or_province, house_id)
-values (1,'Rasulov 12','Bishkek','Gum',5);
+values (5,'Rasulov 12','Bishkek','Gum',5);
 
 
 
