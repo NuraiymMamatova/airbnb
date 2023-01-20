11aefceb918fe81c8879549984f2db53b0815c58
ashymuulusamarbek <samarbekawymuulu@gmail.com>
2023-01-20 12:00:41 +0600

    Please, see the updated swagger.

--- src/main/java/com/example/airbnbb7/api/AuthApi.java
+++ src/main/java/com/example/airbnbb7/api/AuthApi.java
@@ -5,6 +5,8 @@ import com.example.airbnbb7.db.service.serviceImpl.UserServiceImpl;
 import com.example.airbnbb7.dto.request.UserRequest;
 import com.example.airbnbb7.dto.response.LoginResponse;
 import com.google.firebase.auth.FirebaseAuthException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth/")
+@Tag(name = "Auth API", description = "Authentication API")
 public class AuthApi {
 
     private final UserService userService;
@@ -21,11 +24,13 @@ public class AuthApi {
     private final UserServiceImpl authService;
 
     @PostMapping("login")
+    @Operation(summary = "Sign in", description = "Authentication")
     public LoginResponse login(@RequestBody UserRequest request) {
         return userService.login(request);
     }
 
     @PostMapping("/google")
+    @Operation(summary = "Sign up & sign in", description = "via google")
     public LoginResponse authResponse(String tokenId) throws FirebaseAuthException {
         return authService.authWithGoogle(tokenId);
     }
