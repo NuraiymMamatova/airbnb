07f0d8fc4ae734b1406a54399a3377f2461d1e20
Eldiar004 <eldiarro2004@gmail.com>
2023-01-27 16:36:05 +0600

    add swagger for pagination

--- src/main/java/com/example/airbnbb7/api/HouseApi.java
+++ src/main/java/com/example/airbnbb7/api/HouseApi.java
@@ -4,6 +4,7 @@ import com.example.airbnbb7.db.enums.HouseType;
 import com.example.airbnbb7.db.service.serviceImpl.HouseServiceImpl;
 import com.example.airbnbb7.dto.response.HouseResponseSortedPagination;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +16,12 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/houses")
+@Tag(name = "House Api",description = "House Api")
 public class HouseApi {
 
     private final HouseServiceImpl houseService;
 
-    @GetMapping
+    @GetMapping("/pagination")
     @Operation(summary = "House get all pagination",description = "This is get all pagination for houses")
     public List<HouseResponseSortedPagination> findAllHousesPage(@RequestParam(name = "sortOrFilter", required = false) String fieldToSort,
                                                                  @RequestParam(name = "text", required = false) String text,
