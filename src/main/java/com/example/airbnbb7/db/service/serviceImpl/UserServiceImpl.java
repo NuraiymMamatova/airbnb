70389b1dcc0540c5b9490529237ebea1cf0537f8
Zhumgalbek <jumgalbek2002@icloud.com>
2023-02-07 12:54:18 +0300

    create profile

--- src/main/java/com/example/airbnbb7/db/service/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/example/airbnbb7/db/service/serviceImpl/UserServiceImpl.java
@@ -57,13 +57,77 @@ public class UserServiceImpl implements UserService {
 
     private static Long userId;
 
-
     private String email;
 
     private final HouseRepository houseRepository;
 
     public String getEmail() {
         return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @PostConstruct
+    void init() throws IOException {
+        GoogleCredentials googleCredentials =
+                GoogleCredentials.fromStream(new ClassPathResource("airbnb-b7.json").getInputStream());
+        FirebaseOptions firebaseOptions = FirebaseOptions.builder()
+                .setCredentials(googleCredentials)
+                .build();
+        FirebaseApp firebaseApp = FirebaseApp.initializeApp(firebaseOptions);
+    }
+
+    public LoginResponse authWithGoogle(String tokenId) throws FirebaseAuthException {
+        FirebaseToken firebaseToken = FirebaseAuth.getInstance().verifyIdToken(tokenId);
+
+        User user;
+        if (userRepository.findByEmail(firebaseToken.getEmail()).isEmpty()) {
+            user = new User();
+            user.addRole(roleRepository.findByName("USER"));
+            user.setPassword(passwordEncoder.encode(firebaseToken.getEmail()));
+            user.setName(firebaseToken.getName());
+            user.setEmail(firebaseToken.getEmail());
+            setUserId(user.getId());
+            userRepository.save(user);
+        }
+
+        user = userRepository.findByEmail(firebaseToken.getEmail()).orElseThrow(() -> new NotFoundException(String.format("User %s not found!", firebaseToken.getEmail())));
+        setUserId(user.getId());
+        String token = jwtTokenUtil.generateToken(user);
+        return new LoginResponse(user.getEmail(), token, userRepository.findRoleByUserEmail(user.getEmail()).getNameOfRole());
+    }
+
+    public LoginResponse login(@RequestBody UserRequest request) {
+        UsernamePasswordAuthenticationToken token =
+                new UsernamePasswordAuthenticationToken(request.getEmail(),
+                        request.getPassword());
+        User user = userRepository.findByEmail(token.getName()).orElseThrow(
+                () -> {
+                    throw new NotFoundException("the user with this email was not found");
+                });
+        if (request.getPassword() == null) {
+            throw new NotFoundException("Password must not be empty");
+        }
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new BadCredentialsException("invalid password");
+        }
+        setEmail(user.getEmail());
+        setUserId(user.getId());
+        return new LoginResponse(jwtTokenUtil.generateToken(user), user.getEmail(), roleRepository.findRoleByUserId(user.getId()).getNameOfRole());
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("not found email"));
+    }
+
+    public static Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
     }
 
     @Override
@@ -72,17 +136,25 @@ public class UserServiceImpl implements UserService {
         List<Booking> bookings = new ArrayList<>(userRepository.findById(UserRepository.getUserId()).get().getBookings());
         switch (mainInUserProfile) {
             case "Bookings" -> {
-                for (Booking booking:bookings) {
-                    houseResponses.add(houseResponseConverter.viewHouse(booking.getHouse()));
+                for (Booking booking : bookings) {
+                    HouseResponse houseResponse = new HouseResponse(booking.getHouse().getId(), booking.getHouse().getPrice(),
+                            booking.getHouse().getTitle(), booking.getHouse().getDescriptionOfListing(), booking.getHouse().getMaxOfGuests(), booking.getHouse().getHouseType()
+                            , rating.getRating(booking.getHouse().getId()));
+                    houseResponse.setOwner(new UserResponse(booking.getHouse().getOwner().getId(),
+                            booking.getHouse().getOwner().getName(), booking.getHouse().getOwner().getEmail(), booking.getHouse().getOwner().getImage()));
+                    houseResponse.setLocation(new LocationResponse(booking.getHouse().getLocation().getId(),
+                            booking.getHouse().getLocation().getTownOrProvince(), booking.getHouse().getLocation().getAddress(), booking.getHouse().getLocation().getRegion()));
+                    houseResponse.setImages(booking.getHouse().getImages());
+                    houseResponses.add(houseResponse);
                 }
                 return houseResponses;
             }
             case "My announcement" -> {
-                return sortingHousesByRating(houseSorting,sortingHousesByValue,sortingHousesByRating);
+                return sortingHousesByRating(houseSorting, sortingHousesByValue, sortingHousesByRating);
             }
             case "On moderation" -> {
-                for (House house: userRepository.findById(UserRepository.getUserId()).get().getAnnouncements()) {
-                    if (house.getHousesStatus().equals(HousesStatus.ON_MODERATION)){
+                for (House house : userRepository.findById(UserRepository.getUserId()).get().getAnnouncements()) {
+                    if (house.getHousesStatus().equals(HousesStatus.ON_MODERATION)) {
                         houseResponses.add(houseResponseConverter.viewHouse(house));
                     }
                 }
@@ -185,225 +257,5 @@ public class UserServiceImpl implements UserService {
         return houseResponses;
     }
 
-//    @Override
-//    public List<HouseResponse> userProfile(String mainInUserProfile, String houseSorting, String sortingHousesByValue, String sortingHousesByRating) {
-//        List<HouseResponse> houseResponses = new ArrayList<>();
-//        List<Booking> bookings = new ArrayList<>(userRepository.findById(UserRepository.getUserId()).get().getBookings());
-//        switch (mainInUserProfile) {
-//            case "Bookings" -> {
-//                for (Booking booking:bookings) {
-//                    HouseResponse houseResponse = new HouseResponse(booking.getHouse().getId(), booking.getHouse().getPrice(),
-//                            booking.getHouse().getTitle(), booking.getHouse().getDescriptionOfListing(), booking.getHouse().getMaxOfGuests(), booking.getHouse().getHouseType()
-//                    ,rating.getRating(booking.getHouse().getId()));
-//                    houseResponse.setOwner(new UserResponse(booking.getHouse().getOwner().getId(),
-//                            booking.getHouse().getOwner().getName(), booking.getHouse().getOwner().getEmail(), booking.getHouse().getOwner().getImage()));
-//                    houseResponse.setLocation(new LocationResponse(booking.getHouse().getLocation().getId(),
-//                            booking.getHouse().getLocation().getTownOrProvince(), booking.getHouse().getLocation().getAddress(), booking.getHouse().getLocation().getRegion()));
-//                    houseResponse.setImages(booking.getHouse().getImages());
-//                    houseResponses.add(houseResponse);
-//                }
-//                return houseResponses;
-//            }
-//            case "My announcement" -> {
-//                return sortingHousesByRating(houseSorting,sortingHousesByValue,sortingHousesByRating);
-//            }
-//            case "On moderation" -> {
-//                for (House house: userRepository.findById(UserRepository.getUserId()).get().getAnnouncements()) {
-//                    if (house.getHousesStatus().equals(HousesStatus.ON_MODERATION)){
-//                        HouseResponse houseResponse = new HouseResponse(house.getId(), house.getPrice(),
-//                                house.getTitle(), house.getDescriptionOfListing(), house.getMaxOfGuests(), house.getHouseType(),rating.getRating(house.getId()));
-//                        houseResponse.setOwner(new UserResponse(house.getOwner().getId(),
-//                                house.getOwner().getName(), house.getOwner().getEmail(), house.getOwner().getImage()));
-//                        houseResponse.setLocation(new LocationResponse(house.getLocation().getId(),
-//                                house.getLocation().getTownOrProvince(), house.getLocation().getAddress(), house.getLocation().getRegion()));
-//                        houseResponse.setImages(house.getImages());
-//                        houseResponses.add(houseResponse);
-//                    }
-//                }
-//                return houseResponses;
-//            }
-//        }
-//        return null;
-//    }
-//
-//    private List<HouseResponse> houseSorting(String houseSorting) {
-//        List<HouseResponse> houseResponses = new ArrayList<>();
-//        List<HouseResponse> houseResponseList = new ArrayList<>();
-//        for (House house : userRepository.findById(UserRepository.getUserId()).get().getAnnouncements()) {
-//            HouseResponse houseResponse = new HouseResponse(house.getId(), house.getPrice(),
-//                    house.getTitle(), house.getDescriptionOfListing(), house.getMaxOfGuests(), house.getHouseType(),rating.getRating(house.getId()));
-//            houseResponse.setOwner(new UserResponse(house.getOwner().getId(),
-//                    house.getOwner().getName(), house.getOwner().getEmail(), house.getOwner().getImage()));
-//            houseResponse.setLocation(new LocationResponse(house.getLocation().getId(),
-//                    house.getLocation().getTownOrProvince(), house.getLocation().getAddress(), house.getLocation().getRegion()));
-//            houseResponse.setImages(house.getImages());
-//            houseResponses.add(houseResponse);
-//        }
-//        switch (houseSorting) {
-//            case "In wish list" -> {
-//                return houseResponses;
-//            }
-//            case "Apartment" -> {
-//                for (House house : userRepository.findById(UserRepository.getUserId()).get().getAnnouncements()) {
-//                    if (house.getHouseType().toString().equals("APARTMENT")) {
-//                        HouseResponse houseResponse = new HouseResponse(house.getId(), house.getPrice(),
-//                                house.getTitle(), house.getDescriptionOfListing(), house.getMaxOfGuests(), house.getHouseType(),rating.getRating(house.getId()));
-//                        houseResponse.setOwner(new UserResponse(house.getOwner().getId(),
-//                                house.getOwner().getName(), house.getOwner().getEmail(), house.getOwner().getImage()));
-//                        houseResponse.setLocation(new LocationResponse(house.getLocation().getId(),
-//                                house.getLocation().getTownOrProvince(), house.getLocation().getAddress(), house.getLocation().getRegion()));
-//                        houseResponse.setImages(house.getImages());
-//                        houseResponseList.add(houseResponse);
-//                    }
-//                }
-//                return houseResponseList;
-//            }
-//            case "House" -> {
-//                for (House house : userRepository.findById(UserRepository.getUserId()).get().getAnnouncements()) {
-//                    if (house.getHouseType().toString().equals("HOUSE")) {
-//                        HouseResponse houseResponse = new HouseResponse(house.getId(), house.getPrice(),
-//                                house.getTitle(), house.getDescriptionOfListing(), house.getMaxOfGuests(), house.getHouseType(),rating.getRating(house.getId()));
-//                        houseResponse.setOwner(new UserResponse(house.getOwner().getId(),
-//                                house.getOwner().getName(), house.getOwner().getEmail(), house.getOwner().getImage()));
-//                        houseResponse.setLocation(new LocationResponse(house.getLocation().getId(),
-//                                house.getLocation().getTownOrProvince(), house.getLocation().getAddress(), house.getLocation().getRegion()));
-//                        houseResponse.setImages(house.getImages());
-//                        houseResponseList.add(houseResponse);
-//                    }
-//                    return houseResponseList;
-//                }
-//            }
-//        }
-//        return houseResponses;
-//    }
-//
-//    private List<HouseResponse> sortingHousesByValue(String houseSorting, String sortingHousesByValue) {
-//        List<HouseResponse> houseResponses = new ArrayList<>(houseSorting(houseSorting));
-//        switch (sortingHousesByValue) {
-//            case "Low to high" -> {
-//                houseResponses.sort(Comparator.comparing(HouseResponse::getPrice));
-//                return houseResponses;
-//            }
-//            case "High to low" -> {
-//                houseResponses.sort(Comparator.comparing(HouseResponse::getPrice).reversed());
-//                return houseResponses;
-//            }
-//        }
-//        return houseResponses;
-//    }
-//
-//    private List<HouseResponse> sortingHousesByRating(String houseSorting, String sortingHousesByValue, String sortingHousesByRating) {
-//        List<HouseResponse> houseResponses = new ArrayList<>(sortingHousesByValue(houseSorting, sortingHousesByValue));
-//        List<HouseResponse> houseResponseList = new ArrayList<>();
-//        switch (sortingHousesByRating) {
-//            case "One" -> {
-//                for (HouseResponse house : houseResponses) {
-//                    if (house.getRating() > 0 && house.getRating() <= 1) {
-//                        houseResponseList.add(house);
-//                    }
-//                }
-//                return houseResponseList;
-//            }
-//            case "Two" -> {
-//                for (HouseResponse house : houseResponses) {
-//                    if (house.getRating() > 1 && house.getRating() <= 2) {
-//                        houseResponseList.add(house);
-//                    }
-//                }
-//                return houseResponseList;
-//            }
-//            case "Three" -> {
-//                for (HouseResponse house : houseResponses) {
-//                    if (house.getRating() > 2 && house.getRating() <= 3) {
-//                        houseResponseList.add(house);
-//                    }
-//                }
-//                return houseResponseList;
-//            }
-//            case "Four" -> {
-//                for (HouseResponse house : houseResponses) {
-//                    if (house.getRating() > 3 && house.getRating() <= 4) {
-//                        houseResponseList.add(house);
-//                    }
-//                }
-//                return houseResponseList;
-//            }
-//            case "Five" -> {
-//                for (HouseResponse house : houseResponses) {
-//                    if (house.getRating() > 4 && house.getRating() <= 5) {
-//                        houseResponseList.add(house);
-//                    }
-//                }
-//                return houseResponseList;
-//            }
-//        }
-//        return houseResponses;
-//    }
 
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @PostConstruct
-    void init() throws IOException {
-        GoogleCredentials googleCredentials =
-                GoogleCredentials.fromStream(new ClassPathResource("airbnb-b7.json").getInputStream());
-        FirebaseOptions firebaseOptions = FirebaseOptions.builder()
-                .setCredentials(googleCredentials)
-                .build();
-        FirebaseApp firebaseApp = FirebaseApp.initializeApp(firebaseOptions);
-    }
-
-    public LoginResponse authWithGoogle(String tokenId) throws FirebaseAuthException {
-        FirebaseToken firebaseToken = FirebaseAuth.getInstance().verifyIdToken(tokenId);
-
-        User user;
-        if (userRepository.findByEmail(firebaseToken.getEmail()).isEmpty()) {
-            user = new User();
-            user.addRole(roleRepository.findByName("USER"));
-            user.setPassword(passwordEncoder.encode(firebaseToken.getEmail()));
-            user.setName(firebaseToken.getName());
-            user.setEmail(firebaseToken.getEmail());
-            setUserId(user.getId());
-            userRepository.save(user);
-        }
-
-        user = userRepository.findByEmail(firebaseToken.getEmail()).orElseThrow(() -> new NotFoundException(String.format("User %s not found!", firebaseToken.getEmail())));
-        setUserId(user.getId());
-        String token = jwtTokenUtil.generateToken(user);
-        return new LoginResponse(user.getEmail(), token, userRepository.findRoleByUserEmail(user.getEmail()).getNameOfRole());
-    }
-
-    public LoginResponse login(@RequestBody UserRequest request) {
-        UsernamePasswordAuthenticationToken token =
-                new UsernamePasswordAuthenticationToken(request.getEmail(),
-                        request.getPassword());
-        User user = userRepository.findByEmail(token.getName()).orElseThrow(
-                () -> {
-                    throw new NotFoundException("the user with this email was not found");
-                });
-        if (request.getPassword() == null) {
-            throw new NotFoundException("Password must not be empty");
-        }
-        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
-            throw new BadCredentialsException("invalid password");
-        }
-        setEmail(user.getEmail());
-        setUserId(user.getId());
-        return new LoginResponse(jwtTokenUtil.generateToken(user), user.getEmail(), roleRepository.findRoleByUserId(user.getId()).getNameOfRole());
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("not found email"));
-    }
-
-    public static Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
 }
