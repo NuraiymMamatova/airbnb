8b1977fa31cbe77eb9c91ea24d75ecd19256ce36
Zhumgalbek <jumgalbek2002@icloud.com>
2023-02-08 19:30:44 +0300

    corrected the mistake!

--- src/main/java/com/example/airbnbb7/api/UserApi.java
+++ src/main/java/com/example/airbnbb7/api/UserApi.java
@@ -1,14 +1,14 @@
 package com.example.airbnbb7.api;
 
 import com.example.airbnbb7.db.service.UserService;
-import com.example.airbnbb7.dto.response.HouseResponse;
 import com.example.airbnbb7.dto.response.ProfileResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
--- src/main/java/com/example/airbnbb7/dto/response/ProfileResponse.java
+++ src/main/java/com/example/airbnbb7/dto/response/ProfileResponse.java
@@ -1,6 +1,5 @@
 package com.example.airbnbb7.dto.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
