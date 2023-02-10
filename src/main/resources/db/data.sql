76eed244579d4fbf5e22b1545d3c08c6645e9cd4
Zhumgalbek <jumgalbek2002@icloud.com>
2023-02-10 17:29:08 +0300

    added pagination

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -27,7 +27,7 @@ insert into houses(id, price, title, description_of_listing, max_of_guests, book
                    house_type, houses_status,is_favorite, houses_booked, date_house_created, owner_id)
 values (2, 15, 'Ihlas', 'Tit box, a flat-screen TV and a shared bathroom with a shower. At Garden' ||
                         ' Hotel & SPA the rooms have bed linen and towels.
-    ', 10, 1, 1, 2,false, 1, null, 2);
+    ', 10, 1, 1, 2,false, 1, null, 1);
 
 insert into houses(id, price, title, description_of_listing, max_of_guests, bookings,
                    house_type, houses_status,is_favorite, houses_booked, date_house_created, owner_id)
