0034bec733171e1d457298391a21ff24ec5d98e1
Zhumgalbek <jumgalbek2002@icloud.com>
2023-02-13 19:48:12 +0300

    update main

--- src/main/java/com/example/airbnbb7/db/repository/HouseRepository.java
+++ src/main/java/com/example/airbnbb7/db/repository/HouseRepository.java
@@ -1,7 +1,6 @@
 package com.example.airbnbb7.db.repository;
 
 import com.example.airbnbb7.db.entities.House;
-import com.example.airbnbb7.dto.response.*;
 import com.example.airbnbb7.dto.response.AnnouncementResponseForAdmin;
 import com.example.airbnbb7.dto.response.AnnouncementResponseForUser;
 import com.example.airbnbb7.dto.response.AnnouncementResponseForVendor;
@@ -10,7 +9,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
