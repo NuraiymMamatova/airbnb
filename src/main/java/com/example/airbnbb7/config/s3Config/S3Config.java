3ac71c9fd06936f9d1da39230db93859f75fafea
Nurisa <nurisa.abdykalykova03@gmail.com>
2023-02-14 18:05:26 +0600

    add region value

--- src/main/java/com/example/airbnbb7/config/s3Config/S3Config.java
+++ src/main/java/com/example/airbnbb7/config/s3Config/S3Config.java
@@ -21,9 +21,12 @@ public class S3Config {
     @Value("${aws.secret.key}")
     private String secretKey;
 
+    @Value("${AWS_REGION}")
+    private String REGION;
+
     @Bean
     S3Client s3Client() {
-        Region region = Region.EU_CENTRAL_1;
+        Region region = Region.of(REGION);
         return S3Client.builder()
                 .region(region)
                 .credentialsProvider(StaticCredentialsProvider
