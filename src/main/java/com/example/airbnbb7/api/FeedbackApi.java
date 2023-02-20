f89742d1126ea27ed1ed9c7bef7ee50900a0b5c7
Eldiar004 <eldiarro2004@gmail.com>
2023-02-20 13:09:24 +0600

    annotation @CrossOrigin and @Operation added into FeedbackApi

--- src/main/java/com/example/airbnbb7/api/FeedbackApi.java
+++ src/main/java/com/example/airbnbb7/api/FeedbackApi.java
@@ -4,6 +4,7 @@ import com.example.airbnbb7.db.customClass.SimpleResponse;
 import com.example.airbnbb7.db.service.FeedbackService;
 import com.example.airbnbb7.dto.request.FeedbackRequestForSave;
 import com.example.airbnbb7.dto.request.FeedbackRequestForUpdate;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -11,11 +12,13 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/feedback")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class FeedbackApi {
 
     private final FeedbackService feedbackService;
 
     @PostMapping("/{houseId}")
+    @Operation(summary = "SAVE FEEDBACK" , description = "THIS ENDPOINT RESPONSIBLE FOR SAVING FEEDBACK")
     public SimpleResponse save(Authentication authentication, @RequestBody FeedbackRequestForSave feedbackRequestForSave,
                                @PathVariable Long houseId) {
 
@@ -23,11 +26,13 @@ public class FeedbackApi {
     }
 
     @DeleteMapping("/delete/{feedbackId}")
+    @Operation(summary = "DELETE FEEDBACK" , description = "THIS ENDPOINT RESPONSIBLE FOR DELETING FEEDBACK")
     public SimpleResponse delete(@PathVariable Long feedbackId, Authentication authentication) {
         return feedbackService.deleteFeedback(authentication, feedbackId);
     }
 
     @PutMapping("/update/{feedbackId}")
+    @Operation(summary = "UPDATE FEEDBACK" , description = "THIS ENDPOINT RESPONSIBLE FOR UPDATING FEEDBACK")
     public SimpleResponse update(@PathVariable Long feedbackId,
                                  @RequestBody FeedbackRequestForUpdate feedbackRequest,
                                  Authentication authentication) {
