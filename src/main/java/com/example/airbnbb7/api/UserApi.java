e2f8e6394d932b4d3e832d6bc727f98ff970ee4d
Nurisa <nurisa.abdykalykova03@gmail.com>
2023-02-24 16:52:50 +0600

    delete params names

--- src/main/java/com/example/airbnbb7/api/UserApi.java
+++ src/main/java/com/example/airbnbb7/api/UserApi.java
@@ -23,14 +23,14 @@ public class UserApi {
 
     @GetMapping
     @Operation(summary = "User Profile", description = "With this method, you can see your houses and booked houses. Houses that are already being checked by the administrator.")
-    public ProfileResponse userProfile(@RequestParam(name = "Select one of these Lists Bookings, My announcement, On moderation", required = false) String mainInUserProfile,
-                                       @RequestParam(name = "Here you can sort as you wish(In wish list)", required = false) String sortHousesAsDesired,
-                                       @RequestParam(name = "Here you can sort the apartment(Apartment)", required = false) String sortHousesByApartments,
-                                       @RequestParam(name = "Here you can sort the house(House)", required = false) String sortHousesByHouses,
-                                       @RequestParam(name = "Here you can sort by price", required = false) String sortingHousesByValue,
-                                       @RequestParam(name = "Here you can sort by rating", required = false) String sortingHousesByRating,
-                                       @RequestParam(name = "Here you can write which page you want to open") int page,
-                                       @RequestParam(name = "Here you can write how many objects should be on one page") int size,
+    public ProfileResponse userProfile(@RequestParam(required = false) String mainInUserProfile,
+                                       @RequestParam(required = false) String sortHousesAsDesired,
+                                       @RequestParam(required = false) String sortHousesByApartments,
+                                       @RequestParam(required = false) String sortHousesByHouses,
+                                       @RequestParam(required = false) String sortingHousesByValue,
+                                       @RequestParam(required = false) String sortingHousesByRating,
+                                       @RequestParam int page,
+                                       @RequestParam int size,
                                        Authentication authentication) {
         return userService.userProfile(mainInUserProfile, sortHousesByApartments, sortHousesByHouses, sortHousesAsDesired, sortingHousesByValue, sortingHousesByRating, authentication, page, size);
     }
