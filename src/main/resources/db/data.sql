4f4007f9d0072bf7b9322bef2964cb74a9ec3050
Eldiar004 <eldiarro2004@gmail.com>
2023-02-28 14:23:43 +0600

    start commit

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -27,71 +27,89 @@ insert into houses(id, price, title, description_of_listing, max_of_guests, book
                    house_type, houses_status, is_favorite, houses_booked, date_house_created, owner_id)
 values (1, 20, 'Asman', 'flat-screen TV and a shared bathroom with a shower.' ||
                         ' At Garden Hotel & SPA the rooms have bed linen and towels.
-    ', 5, 0, 1, 2,false, 1, null, 2);
+    ', 5, 0, 1, 2,false, 1, '2023-09-13 18:20:03', 2);
 
 insert into houses(id, price, title, description_of_listing, max_of_guests, bookings,
                    house_type, houses_status,is_favorite, houses_booked, date_house_created, owner_id)
 values (2, 15, 'Ihlas', 'Tit box, a flat-screen TV and a shared bathroom with a shower. At Garden' ||
                         ' Hotel & SPA the rooms have bed linen and towels.
-    ', 10, 1, 1, 2,false, 1, null, 2);
+    ', 10, 1, 1, 2,false, 1, '2023-09-13 18:20:03', 2);
 
 insert into houses(id, price, title, description_of_listing, max_of_guests, bookings,
                    house_type, houses_status,is_favorite, houses_booked, date_house_created, owner_id)
 values (3, 29, 'Elite', ' and towels.
-', 10, 1, 1, 2,false, 1, null, 1);
+', 10, 1, 1, 2,false, 1, '2023-09-13 18:20:03', 1);
 
 insert into houses(id, price, title, description_of_listing, max_of_guests, bookings,
                    house_type, houses_status,is_favorite, houses_booked, date_house_created, owner_id)
 values (4, 10, 'Delmar', ' boa shared bathroom with a shower. At Garden ' ||
                          'Hotel & SPA the rooms have bed linen and towels.
-    ', 10, 1, 0, 2, false,1, null, 2);
+    ', 10, 1, 0, 2, false,1,'2023-09-13 18:20:03', 2);
 
 insert into houses(id, price, title, description_of_listing, max_of_guests, bookings,
                    house_type, houses_status,is_favorite, houses_booked, date_house_created, owner_id)
-values (5, 40, 'Best', 'e bed linen and towels.', 10, 1, 0, 2, false,1, null, 2);
+values (5, 40, 'Best', 'e bed linen and towels.', 10, 1, 0, 2, false,1, '2023-09-13 18:20:03', 2);
+
+insert into houses(id, price, title, description_of_listing, max_of_guests, bookings,
+                   house_type, houses_status, is_favorite, houses_booked, date_house_created, owner_id)
+values (6, 20, 'Comfort', 'Very good house', 6, 0, 1, 2,false, 0, '2023-09-13 18:20:03', 2);
+
+insert into houses(id, price, title, description_of_listing, max_of_guests, bookings,
+                   house_type, houses_status, is_favorite, houses_booked, date_house_created, owner_id)
+values (7, 20, 'Ololo', 'Very good house', 3, 0, 1, 2,false, 0, '2023-09-13 18:20:03', 2);
+
 
 insert into locations(id, address, region, town_or_province, house_id)
-values (1,'Toktogul 12','Bishkek','Goin',1);
+values (1,'Toktogul 12','Chui','Goin',1);
 
 insert into locations(id, address, region, town_or_province, house_id)
 values (2,'Kurmanjan 36','Osh','Ozgon',2);
 
 insert into locations(id, address, region, town_or_province, house_id)
-values (3,'Saburov 19','Bishkek','Vefa',3);
+values (3,'Saburov 19','Naryn','Vefa',3);
 
 insert into locations(id, address, region, town_or_province, house_id)
-values (4,'Pushkin 12','Bishkek','Red',4);
+values (4,'Pushkin 12','Issyk-Kul','Red',4);
 
 insert into locations(id, address, region, town_or_province, house_id)
-values (5,'Rasulov 12','Bishkek','Gum',5);
+values (5,'Rasulov 12','Talas','Gum',5);
 
+insert into locations(id, address, region, town_or_province, house_id)
+values (6,'Rasulov 20','JalalAbad','Gum',6);
+
+insert into locations(id, address, region, town_or_province, house_id)
+values (7,'Tolstoy 43','Batken','Gum',7);
 
 insert into house_images(house_id, images)
-values (1, 'image');
+values (1, 'https://images.unsplash.com/photo-1568605114967-8130f3a36994?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8aG91c2V8ZW58MHx8MHx8&w=1000&q=80');
 insert into house_images(house_id, images)
-values (2, 'image');
-
+values (2, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRh_1qqXmUX9CCTv5xw9UFTx5eZwR9w63SjA&usqp=CAU');
 insert into house_images(house_id, images)
-values (3, 'image');
+values (3, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ18pcad8DDEjL0Jw2sImcnYPQ4lRUOPitbvQ&usqp=CAU');
 insert into house_images(house_id, images)
-values (4, 'image');
+values (4, 'https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWFuc2lvbnxlbnwwfHwwfHw%3D&w=1000&q=80');
 insert into house_images(house_id, images)
 values (5, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbGoeZg06hhqGsjJ-DrxjgEmU5o7Jua2vB9Q&usqp=CAU');
+insert into house_images(house_id, images)
+values (6, 'https://images.adsttc.com/media/images/63c5/648e/760d/d201/7078/a81d/medium_jpg/bernaung-house-rad-plus-ar-research-artistic-design-plus-architecture_11.jpg?1673880794');
+insert into house_images(house_id, images)
+values (7, 'https://www.dhanvibuilders.com/wp-content/uploads/2022/07/28-1.jpg');
+
 
 insert into booking_dates(id, check_in, check_out, price, house_id)
-values (1, null, null, 20, 1);
+values (1, '2023-09-13 18:20:03', '2023-09-23 18:20:03', 20, 1);
 
 insert into booking_dates(id, check_in, check_out, price, house_id)
-values (2, null, null, 15, 2);
+values (2, '2023-09-30 18:20:03', '2023-10-5 18:20:03', 15, 2);
 
 insert into booking_dates(id, check_in, check_out, price, house_id)
-values (3, null, null, 29, 3);
+values (3, '2023-09-3 18:20:03', '2023-09-7 18:20:03', 29, 3);
 
 insert into booking_dates(id, check_in, check_out, price, house_id)
-values (4, null, null, 10, 4);
+values (4, '2023-08-2 18:20:03', '2023-09-3 18:20:03', 10, 4);
 
 insert into booking_dates(id, check_in, check_out, price, house_id)
-values (5, null, null, 40, 5);
+values (5, '2023-07-7 18:20:03', '2023-07-15 18:20:03', 40, 5);
 
 insert into feedbacks(id, created_feedback, dislike, likes, rating, text, house_id, user_id)
 values (1, now(), 0, 5, 4, 'this was one of the nicest places we stayed in the four weeks w...', 1, 2);
